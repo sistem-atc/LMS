c90d66b36bff834594d43c954e01e57fa59eb5ad
Kleber <sistem_atc@gmail.com>
2025-01-22 19:20:02 -0300

    WIP

--- database/seeders/CteSeeder.php
+++ database/seeders/CteSeeder.php
@@ -4,8 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Cte;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class CteSeeder extends Seeder
 {
--- database/seeders/PaymentTermSeeder.php
+++ database/seeders/PaymentTermSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\PaymentTerm;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class PaymentTermSeeder extends Seeder
